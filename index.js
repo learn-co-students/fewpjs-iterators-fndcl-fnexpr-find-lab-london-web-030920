3ac41b1b1f73b235364e3d568ed07d08a3111d90
BenDunjay <bendunjay@outlook.com>
2020-04-26 18:37:40 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const testVar = {}
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
 
-function testFunc() {
-  return "hi"
-}
+
+
+function superbowlWin(array){
+  const winningTeam = array.find(element => element.result === "W")
+  return winningTeam ? winningTeam.year : undefined
+  }
+
+
+
+
+
+
+
+
+
+
+
